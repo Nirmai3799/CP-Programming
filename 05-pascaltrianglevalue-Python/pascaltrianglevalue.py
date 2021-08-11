e78af5d5446ed9260fc611954471ce87e6724971
Nirmai3799 <nirmaialuri@msitprogram.net>
2021-08-11 22:20:54 +0530

    version-1

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -4,9 +4,35 @@
 # Triangle where the triangle starts at row 0, and 
 # each row starts at column 0. If either row or col 
 # are not legal values, return None, instead of crashing. 
-
+def printPascal(n) :
+	matrix=[]
+	for line in range(0, n) :
+		a=[]
+		for i in range(0, line + 1) :
+			a.append(binomialCoeff(line, i))
+		matrix.append(a)
+	return matrix
+     
+def binomialCoeff(n, k) :
+    res = 1
+    if (k > n - k) :
+        k = n - k
+    for i in range(0 , k) :
+        res = res * (n - i)
+        res = res // (i + 1)
+     
+    return res
 
 
 
 def fun_pascaltrianglevalue(row, col):
-	return 1
+	a=printPascal(row+1)
+	for i in range(len(a)):
+		for j in range(i+1):
+			if(i==row and j==col):
+				return a[i][j]
+			else:
+				continue
+	return 0
+	
+# print(fun_pascaltrianglevalue(int(input()),int(input())))
