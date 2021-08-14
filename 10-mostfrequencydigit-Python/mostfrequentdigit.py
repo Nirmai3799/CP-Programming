efd86bc25ea8b07531670aac27cb23b4e9233675
Nirmai3799 <nirmaialuri@msitprogram.net>
2021-08-14 13:55:03 +0530

    mfd

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -4,4 +4,27 @@
 
 def mostfrequentdigit(n):
 	# your code goes here
-	pass
+	n=str(n)
+	dict1={}
+	for i in n:
+		if(i not in dict1):
+			dict1[i]=1
+		else:
+			dict1[i]+=1
+	maximum=0
+	val=''
+	# print(dict1)
+	for i in dict1:
+		if(maximum<dict1[i]):
+			maximum=dict1[i]
+			val=i
+		elif(maximum==dict1[i]):
+			if(int(val)>int(i)):
+				val=i 
+			
+		else:
+			continue
+	return int(val)
+# print(mostfrequentdigit(int(input())))
+
+			
