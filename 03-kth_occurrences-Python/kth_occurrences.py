0dfb03340088683211df8731de431b671db7c9c7
Nirmai3799 <nirmaialuri@msitprogram.net>
2021-08-15 19:03:44 +0530

    kth-occurences

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -2,9 +2,20 @@
 # frequent character in the string. If there are multiple characters that 
 # can account as K-th the most frequent character then, print any one of them.
 
-
+import operator
 
 def fun_kth_occurrences(s, n):
-	return 'a'
+	maximum=0
+	dict1={}
+	for i in s:
+		if i not in dict1:
+			dict1[i]=1
+		else:
+			dict1[i]+=1
+	# print(dict1)
+	a=sorted(dict1.items(),key=operator.itemgetter(1),reverse=True)
+	return a[n-1][0]
+	
+
 
 
