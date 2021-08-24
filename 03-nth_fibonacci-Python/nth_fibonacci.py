b6cf93df0e6f3769edd61fe0ab64db2aad585424
Nirmai3799 <nirmaialuri@msitprogram.net>
2021-08-24 19:35:57 +0530

    fibonacci

--- 03-nth_fibonacci-Python/nth_fibonacci.py
+++ 03-nth_fibonacci-Python/nth_fibonacci.py
@@ -6,7 +6,14 @@
 # nthfibonaccinumber(n) that takes a non-negative int n and returns the nth Fibonacci number.
 
 
-
 def fun_nthfibonaccinumber(n):
-	# your code goes Here
-	return 0
+  a=1
+  b=1
+  if n==0:
+    return b
+  else:
+    for i in range(0,n-1):
+      c=a+b
+      a=b
+      b=c
+    return b
