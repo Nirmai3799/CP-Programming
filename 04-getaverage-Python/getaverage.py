9c44b89bb1a94e640970abf5055a4195619745ae
Nirmai3799 <nirmaialuri@msitprogram.net>
2021-08-24 19:39:14 +0530

    getaverage

--- 04-getaverage-Python/getaverage.py
+++ 04-getaverage-Python/getaverage.py
@@ -5,10 +5,19 @@
 # should be ignored. If there are no integer values, return 0 (do not crash here).
 # For example, getAverage('13,excused,14,absent') ignores the two 
 # strings and averages 13 and 14 to return 13.5. Also, getAverage('a,b,c') returns 0.
-
-
-
-
-def fun_getaverage(s): 
-	return 0.0
+def fun_getaverage(s):
+  sum=0.0
+  count=0
+  counte=0
+  p=s.split(",")
+  for i in range(len(p)):
+    if(p[i].isnumeric()):
+      sum+=int(p[i])
+      count+=1
+    else:
+      counte+=1
+      if(counte==len(p)):
+        return 0.0
+  l=sum/count
+  return l
 
